1232052d3078c14ed8336fe250b4404b70beba8b
oysteinfc <52486371+oysteinfc@users.noreply.github.com>
2019-07-22 08:27:59 +0200

    Add files via upload
    JS file for the automatic slideshow in the studentprojects site

---
+++ autoSlides.js
@@ -0,0 +1,15 @@
+//Script for the slideshow menu on the main page. Code from https://www.w3schools.com/howto/howto_js_slideshow.asp
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none"; 
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1} 
+  slides[slideIndex-1].style.display = "block"; 
+  setTimeout(showSlides, 5000); // Change image every 2 seconds
+}
