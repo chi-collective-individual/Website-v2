150228dc4001312b90d670963f3d12e7f6459a50
oysteinfc <52486371+oysteinfc@users.noreply.github.com>
2019-07-30 09:30:29 +0200

    Add files via upload
    Example of how it works to implement the header as an external php file.

---
+++ navbar.php
@@ -0,0 +1,15 @@
+<header class="w3-theme w3-row">
+
+<div class="w3-dropdown-hover w3-col" style="width:50px">
+  <button class="w3-button w3-theme-l2"><i class="material-icons">menu</i></button>
+  <div class="w3-dropdown-content w3-bar-block w3-border">
+    <a href="frontPage.php" class="w3-bar-item w3-button">Hjem</a>
+    <a href="forskningsassistent.php" class="w3-bar-item w3-button">Forskningsassistent</a>
+    <a href="forskningsprosjekter.php" class="w3-bar-item w3-button">Forskningsprosjekter</a>
+    <a href="emneutvikler.php"class="w3-bar-item w3-button">Emneutvikler</a>
+    <a href="studentprosjekter.php"class="w3-bar-item w3-button">Studentprosjekter</a>
+  </div>
+</div>
+
+<div class="w3-rest w3-container"><h1 style="text-align:center"><a href="frontPage.php">Students as Partners</a></h1></div>
+</header>
