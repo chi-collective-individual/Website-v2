cbfe1542d8c63545b722b280a01579be6aa42a6b
Ane Vigre Håland <anevh@stud.ntnu.no>
2019-08-05 08:14:33 +0200

    Upload all changes

---
+++ W2/index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html land="no-nb">
+    
+                
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="stylesheets/styles.css">
+        <link rel="stylesheet" href=""> 
+        <link href="https://fonts.googleapis.com/css?family=Raleway&" rel="stylesheet"><!--stylesheet for font family here-->
+        <script src="scripts/slideshow.js" defer></script>
+
+        <title>Students as Partners</title>
+    </head>
+    <body>
+        
+        <?php include("./menu.php";) ?>
+        <!--container for the header that is in all pages-->
+        <div class="indent">
+            <img src="images/facultyFocus.jpg">
+
+            <!--Information about SaP-->
+            <h1>Students as Partners</h1><p class="center">Ønsker du å samarbeide med et av Norges fremste forskningsmiljøer? Her vil ansatte på Institutt for fysikk legge ut prosjekter for studenter fra alle årskull. Vil du utvikle dine egne idéer, eller bidra i et av de mange studentprosjektene på NTNU? Vi hjelper deg å ta steget!</p>
+            
+            <!--Information about the different types of projects with link to their pages-->
+            <div class="bg-blue">
+                <h1>Hva kan du jobbe med?</h1>
+                <div class="flex-container">
+                    <a href="forskningsassistent/index.html" title="Forskningsassistent" class="two-split"><div class="hover-shadow"><h3>Forskningsassistent</h3><p>Lurer du på hvordan man bruker avansert teori i nyskapende forskning? Ved å være forskningsassistent ved Institutt for fysikk vil du arbeide med vitenskapelig ansatte i deres forskningsprosjekter. Les mer.</p></div></a>
+
+                    <a href="forskningsprosjekter/index.html" title="Forskningsprosjekter" class="two-split mt"><div class="hover-shadow"><h3>Forskningsprosjekter</h3><p>Hvis du ønsker å prøve deg som forsker er dette perfekt for deg! Du får muligheten til å arbeide med ditt eget prosjekt. Prosjektene varierer mye, fra numerisk analyse til å reprodusere eksperimentelle forsøk. Les mer.</p></div></a>
+
+                    <a href="emneutvikler/index.html" title="Emneutvikler" class="two-split"><div class="hover-shadow"><h3>Emneutvikler</h3><p>Vil du bidra til å utvikle læringsressurser for kommende studentkull? Ønsker du å få mer kunnskap om god pedagogisk praksis og å videreutvikle undervisningen av fysikk? Les mer.</p></div></a>
+
+                    <a href="studentprosjekter/index.html" title="Studentprosjekter" class="two-split mt"><div class="hover-shadow"><h3>Studentprosjekter</h3><p>Brenner du inne med en idé du ønsker å realisere? Vil du samarbeide med andre studenter og anvende dine praktiske og analytiske ferdigheter? Her finner du tidligere prosjekter, nåværende prosjekter og eksterne prosjekter. Les mer.</p></div></a>
+                </div>
+            </div>
+            
+            <!--News, keep this down to two or three-->
+            <div class="bg-smaragd">
+            <h1 class="mt">Nyheter</h1>
+            <div class="flex-container">
+               <p class="split-narrow">Alenka har jobbet som forskningsassistent for Justin Wells de siste somrene. Les mer om hennes arbeidsoppgaver og erfaringer.<br><a href="nyheter/intervjuAlenka.html" class="white-arrow">&rarr;</a></p>
+               <p class="split-narrow">Håvard fikk sommerjobb hos Justin Wells. Les hvilke erfaringer han tok med seg etter en sommer med forskningsprosjekt.<br><a href="nyheter/intervjuHavard.html" class="white-arrow">&rarr;</a></p>
+            </div>
+            </div>
+            <!--Information about SaP in english-->
+            <h1 class="mt">English information</h1>
+            <div class="flex-container">
+                <div class="two-split">
+                    <img src="images/alenka.jpg"><!--insert image here-->
+                </div>
+
+                <div class="two-split">
+                        
+                        <p>The student research initative is a project developed by the Department of Physics in an attempt to make students and staff at the department work closer together in an early phase of the studies. One of the incentives to close the gap between students and lecturers is to give students the opportunity to work closer with the professors and other staffs research before they choose their masters project. Some members of the staff already include interested students in projects, however there are only a few students that are aware of this opportunity today. With this webpage we hope more students get knowledge of these opportunities and choose to take contact with their lecturers and other members of the staff.</p>
+                        <p>Another aspect of the initative and purpose of this page is to make it easier for students to create and get involved in student projects. The student projects can be everything from small experiments connected with a certain topic in a subject, or big interdisciplinary projects including many students and professors mentoring them.</p>
+                        <p>In a long perspective we hope this again will lead to greater knowledge of applied and theoretical physics. Applying and using the knowledge you gain from class on practical problems and real situations will broaden the understanding of scientific method and research. We also believe that seeing how you can use the knowledge you gain in class will lead to a greater interest in physics, and hope this makes the students even more motivated to learn.</p>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
---
+++ W2/menu.php
@@ -0,0 +1,15 @@
+
+<div class="menu"> 
+            <!--PHP file with the menu and header. Uncomment and delete the menu from here when using php on server.-->
+            
+            <ul>
+                <lh><a href="https://ntnu.no/fysikk" title="Institutt for Fysikk" target="_blank"><img src="images/ifyLogo.jpg"></a></lh>
+                <a href="index.html" title="Students as Partners"><li>Hjem</li></a>
+                <a href="forskningsassistent/index.html" title="Forskningsassistent"><li>Forskningsassistent</li></a>
+                <a href="forskningsprosjekter/index.html" title="Forskningsprosjekter"><li>Forskningsprosjekter</li></a>
+                <a href="emneutvikler/index.html" title="Emneutvikler"><li>Emneutvikler</li></a>
+                <a href="studentprosjekter/index.html" title="Studentprosjekter"><li>Studentprosjekter</li></a>
+                <a href="nyheter/index.html" title="Nyheter"><li>Nyheter</li></a>
+                <a href="kontakt/index.html" title="Kontakt"><li>Kontakt</li></a>
+            </ul>
+</div>
