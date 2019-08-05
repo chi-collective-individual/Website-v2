d71de9115f46b6d5ed13ccf3e6cc8b95f9edfbe7
oysteinfc <52486371+oysteinfc@users.noreply.github.com>
2019-08-05 13:52:49 +0200

    Update index.php

--- W2/kontakt/index.php
+++ W2/kontakt/index.php
@@ -13,23 +13,7 @@
     </head>
     <body>
         <!--container for the header that is in all pages-->
-<<<<<<< HEAD:W2/kontakt/index.html
-        <div class="menu"> 
-            <!--PHP file with the menu and header. Uncomment and delete the menu from here when using php on server. <?php include('menu.php');?> -->
-            <ul>
-                <lh><a href="https://ntnu.no/fysikk" title="Institutt for Fysikk" target="_blank"><img src="../images/ifyLogo.jpg"></a></lh>
-                <a href="../index.html" title="Students as Partners"><li>Hjem</li></a>
-                <a href="../forskningsassistent/index.html" title="Forskningsassistent"><li>Forskningsassistent</li></a>
-                <a href="../forskningsprosjekter/index.html" title="Forskningsprosjekter"><li>Forskningsprosjekter</li></a>
-                <a href="../emneutvikler/index.html" title="Emneutvikler"><li>Emneutvikler</li></a>
-                <a href="../studentprosjekter/index.html" title="Studentprosjekter"><li>Studentprosjekter</li></a>
-                <a href="../nyheter/index.html" title="Nyheter"><li>Nyheter</li></a>
-                <a href="../kontakt/index.html" title="Kontakt"><li>Kontakt</li></a>
-            </ul>
-        </div>
-=======
-        <?php include(""../menu.php"); ?>
->>>>>>> f04732492d3509ee4c42adc17adfa14fc97a7c36:W2/kontakt/index.php
+        <?php include("../menu.php"); ?>
 
         <div class="indent">
             <!--<img src="images/facultyFocus.jpg"> -->
