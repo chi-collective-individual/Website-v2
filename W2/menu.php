9695e652b4f981ce26ac56000b1dcc9829743153
oysteinfc <52486371+oysteinfc@users.noreply.github.com>
2019-08-05 13:51:06 +0200

    Update menu.php

--- W2/menu.php
+++ W2/menu.php
@@ -1,12 +1,12 @@
 <div class="menu"> 
   <ul>
     <lh><a href="https://ntnu.no/fysikk" title="Institutt for Fysikk" target="_blank"><img src="../images/ifyLogo.jpg"></a></lh>
-    <a href=".index.html" title="Students as Partners"><li>Hjem</li></a>
-    <a href="../forskningsassistent/index.html" title="Forskningsassistent"><li>Forskningsassistent</li></a>
-    <a href="../forskningsprosjekter/index.html" title="Forskningsprosjekter"><li>Forskningsprosjekter</li></a>
-    <a href="../emneutvikler/index.html" title="Emneutvikler"><li>Emneutvikler</li></a>
-    <a href="../studentprosjekter/index.html" title="Studentprosjekter"><li>Studentprosjekter</li></a>
-    <a href="../nyheter/index.html" title="Nyheter"><li>Nyheter</li></a>
-    <a href="../kontakt/index.html" title="Kontakt"><li>Kontakt</li></a>
+    <a href="../index.php" title="Students as Partners"><li>Hjem</li></a>
+    <a href="../forskningsassistent/index.php" title="Forskningsassistent"><li>Forskningsassistent</li></a>
+    <a href="../forskningsprosjekter/index.php" title="Forskningsprosjekter"><li>Forskningsprosjekter</li></a>
+    <a href="../emneutvikler/index.php" title="Emneutvikler"><li>Emneutvikler</li></a>
+    <a href="../studentprosjekter/index.php" title="Studentprosjekter"><li>Studentprosjekter</li></a>
+    <a href="../nyheter/index.php" title="Nyheter"><li>Nyheter</li></a>
+    <a href="../kontakt/index.php" title="Kontakt"><li>Kontakt</li></a>
   </ul>
 </div>
