7307ba8c115d672f55015127a733be3074ffee62
Ane Vigre Håland <anevh@stud.ntnu.no>
2019-08-30 11:38:51 +0200

    added more projects

--- W2/forskningsprosjekter/index.php
+++ W2/forskningsprosjekter/index.php
@@ -33,7 +33,8 @@
 
                     <a title="Utlysning QuSpin" href="./Utlysning_QuSpin.pdf" target="_blank" class="two-split"><div class="hover-shadow"><h3>Utlysning QuSpin</h3><p>Hvordan ivaretas kvinneandelen i fysikkutdannelser? Det kan virke som at det er et stort frafall kvinner innenfor høyere nivå på fysikk, spesielt innenfor teoretisk fysikk. Vi ønsker å hente inn statistikk på kvinneandel innenfor teoretisk fysikk... Les mer</p></div></a>
 
-                    <a href="./Utlysning_TEM.pdf" target="_blank" title="Forskningsassistent" class="two-split mt"><div class="hover-shadow"><h3>Utlysning TEM: Programming/data processing</h3><p>In this project a student will apply, further refine or rewrite to a tutorial Juypter notebooks for the analysis of transmission electron microscopy data. Through the project you will become familiar with using open-source... Read more.</p></div></a>
+                    <a href="./Utlysning_TEM.pdf" target="_blank" title="Research assistant" class="two-split mt"><div class="hover-shadow"><h3>Utlysning TEM: Programming/data processing</h3><p>In this project a student will apply, further refine or rewrite to a tutorial Juypter notebooks for the analysis of transmission electron microscopy data. Through the project you will become familiar with using open-source... Read more.</p></div></a>
+                    <a href="./electron_propagation.pdf" target="_blank" title="Researh assistant" class="two-split mt"><div class="hover-shadow"><h3>Utlysning TEM: Programming/data processing</h3><p>Electrons propagating from extragalactic sources to Earth may loose energy, as they excite waves in the plasma filling the intergalactic space. In this project, the student will survey rates for this loss mechanism suggested in the literature and implement them in a numerical routine... Read more.</p></div></a>
 
                 </div>
             </div>
