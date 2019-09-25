8745ddec61d059aa85ed6b9384e74b79d547b094
Ane Vigre Håland <anevh@stud.ntnu.no>
2019-09-25 14:04:19 +0200

    arrow

--- W2/index.php
+++ W2/index.php
@@ -72,7 +72,7 @@
                 </div>
             </div>
 
-            <a href="#top-content"><img src="images/up-arrow.PNG" style="display: block; margin-left: auto; margin-right: auto; width: 5%; margin-top:40px;"></a> 
+            <a href="#top-content"><img src="images/up-arrow.PNG" style="display: block; margin-left: auto; margin-right: auto; width: 5%; margin-top:80px;"></a> 
             <!--Footer-->
             <div class="bg-smaragd">
                 <h1 class="mt">Mer informasjon</h1>
