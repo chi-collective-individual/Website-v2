40e3e274bb8560d54a0d30b43019044b62614bad
Ane Vigre Håland <anevh@stud.ntnu.no>
2019-09-25 13:26:58 +0200

    arrow

--- W2/index.php
+++ W2/index.php
@@ -24,10 +24,10 @@
         <?php include("menuFrontPage.php"); ?>
 
         <div class="indent">
-            <img src="images/sapLabFront.jpg">
+            <img src="images/sapLabFront.jpg" id="top-content">
 
             <!--Information about SaP-->
-            <h1 id="sap-title">Students as Partners</h1><p class="center">Ønsker du å samarbeide med et av Norges fremste forskningsmiljøer? Her vil ansatte på Institutt for fysikk legge ut prosjekter for studenter fra alle årskull. Vil du utvikle dine egne idéer, eller bidra i et av de mange studentprosjektene på NTNU? Vi hjelper deg å ta steget!</p>
+            <h1>Students as Partners</h1><p class="center">Ønsker du å samarbeide med et av Norges fremste forskningsmiljøer? Her vil ansatte på Institutt for fysikk legge ut prosjekter for studenter fra alle årskull. Vil du utvikle dine egne idéer, eller bidra i et av de mange studentprosjektene på NTNU? Vi hjelper deg å ta steget!</p>
             <!--Temporary announcement below. REMOVE after one studentassistent is hired-->
             <div class="bg-smaragd">
             <div class="flex-container">
@@ -72,7 +72,7 @@
                 </div>
             </div>
 
-            <a href="#sap-title" style="font-size: 50px; text-align: center;">&uArr;</a>
+            <a href="#top-content" style="font-size: 50px; position: center;">&uArr;</a>
             <!--Footer-->
             <div class="bg-smaragd">
                 <h1 class="mt">Mer informasjon</h1>
