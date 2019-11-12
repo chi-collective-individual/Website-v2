8529015d597db93ce22338b13fff4707294f87cb
AJO <alex.andunge@gmail.com>
2019-11-12 21:32:28 +0100

    Added border-radius style to profile images in "Kontakt"
    Also deleted empty index.html file and unnecessary/unused stylesheet-link in header of index.php

--- W2/kontakt/index.php
+++ W2/kontakt/index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html land="no-nb">
-    
-                
+
+
     <head>
         <!-- Global site tag (gtag.js) - Google Analytics -->
         <script async src="https://www.googletagmanager.com/gtag/js?id=UA-147425276-1"></script>
@@ -14,7 +14,6 @@
         <!--Tag ends-->
         <meta charset="UTF-8">
         <link rel="stylesheet" href="../stylesheets/styles.css">
-        <link rel="stylesheet" href=""> 
         <link href="https://fonts.googleapis.com/css?family=Raleway&" rel="stylesheet"><!--stylesheet for font family here-->
         <script src="scripts/slideshow.js" defer></script>
 
@@ -29,7 +28,7 @@
 
             <!--Contact information-->
             <h1>Kontaktinformasjon til studentassistenter</h1><p class="center">Har du spørsmål til nettsiden eller utlysninger, send en mail eller slå av en prat med studentassistentene som vedlikeholder nettsiden.</p>
-            
+
             <div class="bg-smaragd">
                 <h1 class="mt">Studentassistenter</h1>
                 <div class="flex-container footer" style="text-align:center;">
@@ -38,7 +37,7 @@
                    <p class="split-narrow" >Alexander Johansen Ohrt <br><img src="../images/anon.png"><br> Mail: <a href="mailto:alexaoh@stud.ntnu.no"> alexaoh@stud.ntnu.no </a> <br> Mobil:+47 93836710  </p>
                 </div>
             </div>
-           
+
         </div>
     </body>
 </html>
