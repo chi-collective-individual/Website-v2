814fe2fe5efe87631b6e0e41b22099dc216ffd0d
alexaoh <alex.andunge@gmail.com>
2020-05-24 14:40:07 +0200

    Added FY4250 Astro to "Pågående prosjekter" and updated margin in styles

--- emneutvikler/index.php
+++ emneutvikler/index.php
@@ -1,9 +1,6 @@
 <!DOCTYPE html>
-<html land="no-nb">
-    
-                
+<html land="no-nb">   
     <head>
-        <!-- Global site tag (gtag.js) - Google Analytics -->
         <script async src="https://www.googletagmanager.com/gtag/js?id=UA-147425276-1"></script>
         <script>
         window.dataLayer = window.dataLayer || [];
@@ -11,63 +8,50 @@
         gtag('js', new Date());
         gtag('config', 'UA-147425276-1');
         </script>
-        <!--Tag ends-->
         <meta charset="UTF-8">
         <link rel="stylesheet" href="../stylesheets/styles.css">
         <link rel="stylesheet" href=""> 
-        <link href="https://fonts.googleapis.com/css?family=Raleway&" rel="stylesheet"><!--stylesheet for font family here-->
+        <link href="https://fonts.googleapis.com/css?family=Raleway&" rel="stylesheet">
         <script src="scripts/slideshow.js" defer></script>
 
         <title>SaP - Emneutvikler</title>
     </head>
     <body>
-    <!--Menu-->
     <?php include("../menu.php"); ?> 
       
         <div class="indent">
             <img src="../images/emneutvikler_cellebio4.jpg" alt="picture can't load" id="top-content">
 
-            <!--Information about emneutvikler-->
             <h1>Emneutvikler</h1>
             <p class="center-paragraph">Vil du bidra til å utvikle læringsressurser for kommende studentkull? Ønsker du å få mer kunnskap om god pedagogisk praksis og å videreutvikle undervisningen av fysikk? I Students as Partners (SaP) prosjektet er vi interesserte i å komme i kontakt med studenter som kan tenke seg å bidra til å utvikle morgendagens emner og undervisningen av disse. Du vil samarbeide med faglærere og ressurspersoner innen SaP-prosjektet, som alle jobber mot det samme målet: god undervisning. </p>
             
-            <!--Active project announcements-->
             <div class="bg-blue">
                 <h1>Aktive utlysninger</h1>
                 <p class="center-paragraph">Her publiseres nye prosjekter fortløpende.</p>
                 <div class="flex-container">
-
-                    <!--Mal: 
-                    <a href="" title="" class="two-split"><div class="hover-shadow"><h3></h3><p></p></div></a>
-                    -->
                     <a href="./Project_Jaakko.docx.pdf   " target="_blank" title="Improving Classical Mechanics" class="two-split"><div class="hover-shadow"><h3>Improving course content in Classical Mechanics</h3><p>Help in improving the course content of TFY4345 (exercises, computational project, supplemental material). ​ The project considers two parts ... Show more.</p></div></a>
-                    <a href="./fy3403.pdf" target="_blank" title="Numerical exercises in particle physics" class="two-split mt"><div class="hover-shadow"><h3>Developing numerical exercises for Particle Physics </h3><p>Exercises in particle physics contain often tedious calculations of traces over gamma matrices or rather complicated phase space integrals. Aim of this project is to develop exercises... Show more.</p></div></a>
-                    <a href="./FY2450EmneutviklerAstro.pdf" target="_blank" title="Oppdatering av kompendium i astrofysikk" class="two-split"><div class="hover-shadow"><h3>Oppdatering av kompendium i astrofysikk</h3><p>Eg held på skrive eit kompendium i FY2450 astrofysikk. For å kvalitetssikre dette arbeidet, treng eg ein student som kan lese og gje tilbakemelding. I tillegg skal studenten teikne figurar som illustrasjonar... Les meir.</p></div></a>
-                    <a href="./Prosjekt_videoanalys.pdf" target="_blank" title="Videoanalyse Tracker" class="two-split mt"><div class="hover-shadow"><h3>Videoanalyseprogrammet Tracker</h3><p>Delar av laborationerna vid institutt for fysikk är baserade på videoanalysprogrammet Tracker. Det finns idag ett introduktionshäfte som beskriver programmet och visar på exempel på bruk... Les mer.</p></div></a>
+                    <a href="./fy3403.pdf" target="_blank" title="Numerical exercises in particle physics" class="two-split"><div class="hover-shadow"><h3>Developing numerical exercises for Particle Physics </h3><p>Exercises in particle physics contain often tedious calculations of traces over gamma matrices or rather complicated phase space integrals. Aim of this project is to develop exercises... Show more.</p></div></a>
+                    <a href="./Prosjekt_videoanalys.pdf" target="_blank" title="Videoanalyse Tracker" class="two-split"><div class="hover-shadow"><h3>Videoanalyseprogrammet Tracker</h3><p>Delar av laborationerna vid institutt for fysikk är baserade på videoanalysprogrammet Tracker. Det finns idag ett introduktionshäfte som beskriver programmet och visar på exempel på bruk... Les mer.</p></div></a>
                     <a href="./Studentprosjekt_IFYKJ_LAB.pdf" target="_blank" title="IFY/KJ nettside" class="two-split"><div class="hover-shadow"><h3>Nettside til fysikk/kjemi lab</h3><p>Emnet Fysikk/kjemi på 10 studiepoeng tas i 2. semester av ingeniørstudentene i Trondheim, Ålesund og på Gjøvik. I emnet inngår en laboppgave av moderat omfang tematsk knytet tl mekanikkdelen av emnet. Det er behov for å lage en enkel webside for denne laben.... Les mer.</p></div></a>
-                    <a href="./versjonskontrollsystem.pdf" target="_blank" title="Versjonskontrollsystem" class="two-split mt"><div class="hover-shadow"><h3>Versjonskontroll</h3><p>For å effektivt samarbeide om programvareutvikling brukes det nesten alltid et såkalt versjonskontrollsystem. Et slikt system er Git, og dette er noe flere studenter vil komme borti senere i studiet eller i arbeidslivet. ... Les mer.</p></div></a>
+                    <a href="./versjonskontrollsystem.pdf" target="_blank" title="Versjonskontrollsystem" class="two-split"><div class="hover-shadow"><h3>Versjonskontroll</h3><p>For å effektivt samarbeide om programvareutvikling brukes det nesten alltid et såkalt versjonskontrollsystem. Et slikt system er Git, og dette er noe flere studenter vil komme borti senere i studiet eller i arbeidslivet. ... Les mer.</p></div></a>
                 </div>
             </div>
             
-            <!--Read about ongoing projects, keep this down to two or three-->
             <div class="bg-smaragd">
                 <h1 class="mt">Pågående prosjekter</h1>
                 <div class="flex-container">
-                    <!--Mal:
+                    <!--
                     <p class="split-narrow">...</p><br><a href="nyheter/***.php" class="white-arrow">&rarr;</a></p>
-                    -->
-                    <!--<p class="split-narrow">Rasmus Tranås var med å utvikle de numeriske øvingene i mekanisk fysikk sommmeren 2019. Les om hans sommer ved Institutt for fysikk.<br><a href="../nyheter/intervjuRasmus.php" class="white-arrow">&rarr;</a></p> -->
-                   <p>Ingen pågående prosjekter for øyeblikket.<br><br>
+                    --> 
+                    <div class="split-narrow" ><h3>Oppdatering av kompendium i FY2450 - Astrofysikk </h3><p> Kvalitetssikring av skriving av kompendium. Studenten tegner figurer som illustrasjoner til teksten og lager oppgaver med løsningsforslag til hvert kapittel. </p></div>
                 </div>
             </div>
 
-            <!--Past/outdated project announcements-->
             <div class="bg-blue">
                 <h1 class="mt">Tidligere utlysninger</h1>
                 <p class="center-paragraph">Her kan du lese om tidligere utlysninger. Disse er ikke lenger mulige å søke på, men gir deg en pekepinn på hva som kan utlyses og hvordan utlysningene ser ut. </p>
                 <div class="flex-container">
               
-                <!--Mal for å legge ut ferdig prosjekt med prosjektbeskrivelse og rapport-->    
                     <div class="two-split"> 
                         <h3>Demonstrasjoner i TFY4163</h3>
                         <p><strong>Student: </strong>Michael Robert Kelly</p> 
@@ -99,7 +83,6 @@
                 </div>
             </div>
             <a href="#top-content"><img src="../images/up-arrow.PNG" style="display: block; margin-left: auto; margin-right: auto; width: 5%; margin-top:80px;"></a> 
-            <!--Footer-->
             <?php include("../footer_studentprosjekter.php"); ?>
         </div>
     </body>
