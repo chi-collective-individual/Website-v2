73b0fb7213621fe237e1c82b347fb46d9ffa60a1
alexaoh <alex.andunge@gmail.com>
2020-05-24 15:08:02 +0200

    Fixed spelling in nyheter and deleted old files

--- nyheter/index.php
+++ nyheter/index.php
@@ -1,9 +1,6 @@
 <!DOCTYPE html>
 <html lang="no-nb">
-    
-                
     <head>
-        <!-- Global site tag (gtag.js) - Google Analytics -->
         <script async src="https://www.googletagmanager.com/gtag/js?id=UA-147425276-1"></script>
         <script>
         window.dataLayer = window.dataLayer || [];
@@ -11,43 +8,43 @@
         gtag('js', new Date());
         gtag('config', 'UA-147425276-1');
         </script>
-        <!--Tag ends-->
         <meta charset="UTF-8">
         <link rel="stylesheet" href="../stylesheets/styles.css">
         <link rel="stylesheet" href=""> 
-        <link href="https://fonts.googleapis.com/css?family=Raleway&" rel="stylesheet"><!--stylesheet for font family here-->
+        <link href="https://fonts.googleapis.com/css?family=Raleway&" rel="stylesheet">
         <script src="scripts/slideshow.js" defer></script>
 
         <title>Students as Partners</title>
     </head>
     <body>
-        <!--container for the header that is in all pages-->
         <?php include('../menu.php'); ?>
-
 
         <div class="indent">
             <h1 class="pt-medium" id="top-content">Intervju med emneutvikler Rasmus Tranås</h1>
-            <p class="center-paragraph">Rasmus Tranås jobbet sommeren 2019 som emneutvikler på Institutt for fysikk.<br><br><b>Navn:</b> Rasmus Tranås<br><b>Studie:</b> Ferdig med Fysikk og matematikk, med spesialisering i teknisk fysikk.<br><b>Leder:</b> Magnus Lilledahl og Jakko Akola</p>
+            <p class="center-paragraph">Rasmus Tranås jobbet sommeren 2019 som emneutvikler på Institutt for fysikk.<br><br><b>Navn:</b> Rasmus Tranås<br><b>Studie:</b> Ferdig med Fysikk og matematikk, med spesialisering i teknisk fysikk.<br><b>Ledere:</b> Magnus Lilledahl og Jaakko Akola</p>
             <div class="flex-container">
-                <img src="../images/RasmusLab.jpg" class="two-split" style="height: 850px; margin: auto; min-width:500px;"> <!--min-width will stop weird image formats from occuring.-->
+                <img src="../images/RasmusLab.jpg" class="two-split" style="height: 850px; margin: auto; min-width:500px;">
 
                 <div class="two-split">
+                    <h3> Rasmus hadde følgende å si om sin sommer: </h3>
+                    <p><b>Navn:</b> Rasmus Tranås<br><b>Studie:</b> Ferdig med Fysikk og matematikk, med spesialisering i teknisk fysikk.<br><b>Veiledere:</b> Magnus Lilledahl og Jaakko Akola</p>
                     <p>Dette er første sommeren jeg har jobbet som emneutvikler på instituttet. Det var en fin arbeidserfaring der jeg har jobbet med mye teknologi som jeg er glad i. Arbeidet har gått ut på å utvikle numeriske øvinger i mekanisk fysikk (FY1001) og lage en nettside der læringsressurser tilknyttet emnene på IFY kan samles.</p><br>
-                    <p>Formålet med arbeidet er å skape mer engasjement for numerisk fysikk, samle læringsressursene tilknyttet introduksjonsemnene ved instituttet og vise læringsprogresjonen innen numeriske fysikk. Ved å utforme dedikerte numeriske øviniger vil vi øke fokuset på numerikk som et viktig hjelpemiddel i fysikken. Jeg skrev selv masteroppgave med mye fokus på numeriske simuleringer og synes numerisk fysikk er veldig spennende, så da Jakko som var veilederen min på masteroppgaven nevnte denne muligheten måtte jeg jo slå til! Jeg har fått muligheten til å spre entusiasme rundt numerisk fysikk, og forhåpentligvis gjøre studiene for kommende studenter bedre.</p><br>
+                    <p>Formålet med arbeidet er å skape mer engasjement for numerisk fysikk, samle læringsressursene tilknyttet introduksjonsemnene ved instituttet og vise læringsprogresjonen innen numeriske fysikk. Ved å utforme dedikerte numeriske øviniger vil vi øke fokuset på numerikk som et viktig hjelpemiddel i fysikken. Jeg skrev selv masteroppgave med mye fokus på numeriske simuleringer og synes numerisk fysikk er veldig spennende, så da Jaakko, som var veilederen min på masteroppgaven, nevnte denne muligheten måtte jeg jo slå til! Jeg har fått muligheten til å spre entusiasme rundt numerisk fysikk, og forhåpentligvis gjøre studiene for kommende studenter bedre.</p><br>
                     <p>Selv om jeg har arbeidet med teknologi jeg er vant med å bruke har det vært utfordringer underveis. Tidvis har det vært mye selvstendig arbeid som har krevet stor tro på egne ferdigheter. Sammen med de andre sommerstudentene som arbeidet for instituttet har vi taklet dette bra og hjulpet hverandre med å finne gode løsninger. Dette er en erfaring jeg tar med meg videre, og som jeg tror kommer godt med i fremtiden.</p>
                 </div>
             </div>
 
             <h1 style="padding-top: 100px;">Intervju med forskningsassistent Alenka</h1>
 
-            <p class="center-paragraph">Alenka jobber som forskningsassistent denne sommeren under veileder Justin Wells, som hører til forskningsgruppen QuSpin.</p>
+            <p class="center-paragraph">Alenka jobbet som forskningsassistent sommeren 2019, under veiledning av Justin Wells, som hører til forskningsgruppen QuSpin.</p>
             <div class="flex-container">
                 <img id="Img2" alt="Picture can't load" src="../images/alenka.jpg" class="two-split" style="height: 850px; margin: auto; min-width:500px;"/>  
 
                 <div class="two-split">
-                    <p><b>Navn:</b> Alenka Djarmila Behsen<br><b>Studie:</b> Nanoteknologi, med spesialisering i bionanoteknologi. <br><b>Årskull:</b> Begynner i 5. klasse etter sommeren<br><b>Veileder:</b> Justin Wells på QuSpin</p><br>
+                    <h3> Alenka hadde følgende å si om sin sommer: </h3>
+                    <p><b>Navn:</b> Alenka Djarmila Behsen<br><b>Studie:</b> Nanoteknologi, med spesialisering i bionanoteknologi. <br><b>Årskull:</b> Begynner i 5. klasse høsten 2019<br><b>Veileder:</b> Justin Wells på QuSpin</p><br>
                     <p>Dette er andre sommeren jeg jobber som forskningsassistent på laboratoriet, og årets sommerprosjekt går ut på å fjerne sinkforurensinger i et vakuumsystem. Dette byr på mange utfordringer, og jeg har erfart at ting tar tid på labben! Spesielt når en jobber med vakuumsystem.</p><br>
-                    <p>Sommerprosjektet byr på veldig mange forskjellig oppgaver. Alt fra dataanalyse av prøver i vakuumsystemet, til mer «hands-on» reparasjon av utstyr, som lodding. Det blir ikke alltid så pent, men det fungerer! I dag skal vi prøve å fikse en litt defekt vakuumpumpe. Litt kan jeg prøve meg på selv, men heldigvis er det flere involverte vitenskapelige ansatte på labben. Så selv om veilederen min er på ferie, er det som regel alltid noen en kan spørre om hjelp.</p><br>
+                    <p>Sommerprosjektet byr på veldig mange forskjellig oppgaver. Alt fra dataanalyse av prøver i vakuumsystemet, til mer «hands-on» reparasjon av utstyr, som lodding. Det blir ikke alltid så pent, men det fungerer! I dag skal vi prøve å fikse en litt defekt vakuumpumpe. Litt kan jeg prøve meg på selv, men heldigvis er det flere involverte vitenskapelige ansatte på labben. Selv om veilederen min er på ferie, er det som regel alltid noen en kan spørre om hjelp.</p><br>
                     <p>Jeg lærer noe nytt hver dag, og erfaringene fra sommerprosjektet i fjor samt prosjektet i faget nanoverktøy kommer godt med denne sommeren. Arbeidet på labben er fjernt fra det jeg driver med resten av studiet, men det er veldig fint å få litt bredde, og det kommer trolig godt med senere.</p>
                 </div>
             </div>
@@ -55,18 +52,18 @@
             <h1 style="padding-top: 100px;">Intervju med forskningsassistent Håvard</h1>
 
 
-            <p class="center-paragraph">Håvard jobber som forskningsassistent denne sommeren under veileder Justin Wells, som hører til forskningsgruppen QuSpin.</p>
+            <p class="center-paragraph">Håvard jobbet som forskningsassistent sommeren 2019, under veiledning av Justin Wells, som hører til forskningsgruppen QuSpin.</p>
             <div class="flex-container">
-                <img id="Img2" alt="Picture can't load" src="../images/havard.jpg" class="two-split" style="height: 850px; margin: auto; min-width:500px;">  <!--min-width helps format the image on diff. screen sizes-->
+                <img id="Img2" alt="Picture can't load" src="../images/havard.jpg" class="two-split" style="height: 850px; margin: auto; min-width:500px;">  
 
                 <div class="two-split">
-                    <p><b>Navn:</b> Håvard Bakke<br><b>Studie:</b> Nanoteknologi, med spesialisering i nanoelektronikk.<br><b>Årskull:</b> Begynner i 4. klasse etter sommeren<br><b>Veileder:</b> Justin Wells på QuSpin</p><br>
+                    <h3> Håvard hadde følgende å si om sin sommer: </h3>
+                    <p><b>Navn:</b> Håvard Bakke<br><b>Studie:</b> Nanoteknologi, med spesialisering i nanoelektronikk.<br><b>Årskull:</b> Begynner i 4. klasse høsten 2019<br><b>Veileder:</b> Justin Wells på QuSpin</p><br>
                     <p>Dette er den første sommeren jeg jobber som forskningsassistent i laboratoriet, og prosjektet jeg jobber med er å sette opp et nytt system som skal hente målt trykk i et vakuumkammer. Dette skal det lages backup av, og lagres på en nettside som viser data fra vakuumkammeret. Dette krever en del programmeringsferdigheter innenfor Webteknologi, i tillegg til kjennskap til arduino. Arduino-programmering gjøres med hjelp av eksisterende kunnskaper i python og C++, mens det å opprette en nettside krever litt nye kunnskaper som jeg har måttet lære meg i løpet av sommeren. Jeg har måttet sette meg inn i html, css og javascript, men det finnes heldigvis mange gode kodeeksempler ute på nettet. Og interesse for programmering ligger til grunn for valget av dette prosjektet, ettersom vi fikk en liste med flere oppgaver som vi kunne velge fra.</p><br>
                     <p>Jeg har ikke gjort så mye direkte forskningsarbeid selv, men jeg arbeider jo på labben. Der får jeg se hva de vitenskapelig ansatte gjør og hvilke arbeidsoppgaver som må gjøres. Prosjektet har gitt meg et godt innblikk i en forskers hverdag, og det er svært relevant for studiet mitt ettersom jeg har valgt retningen nanoelektronikk.</p>
                 </div>
             </div>
             <a href="#top-content"><img src="../images/up-arrow.PNG" style="display: block; margin-left: auto; margin-right: auto; width: 5%; margin-top:70px;"></a> 
-            <!--Footer-->
             <?php include("../footer_studentprosjekter.php"); ?>
         </div>
        
