0506a53084bc7bb339ecd89027c29bb8ecc8943d
Victoria Madeleine Bjelland <victormb@stud.ntnu.no>
2020-12-29 02:39:06 -0800

    Added english in the menu

--- menu.php
+++ menu.php
@@ -1,11 +1,11 @@
 <div class="menu"> 
   <ul>
     <lh><a href="https://ntnu.no/fysikk" title="Institutt for Fysikk" target="_blank"><img src="../images/ifyLogo.jpg"></a></lh>
-    <a href="../index.php" title="Prosjekt Scrödinger"><li>Hjem</li></a>
-    <a href="../forskningsprosjekter/index.php" title="Forskningsprosjekter"><li>Forskningsprosjekter</li></a>
-    <a href="../emneutvikler/index.php" title="Emneutvikler"><li>Emneutvikler</li></a>
-    <a href="../studentprosjekter/index.php" title="Studentprosjekter"><li>Studentprosjekter</li></a>
-    <a href="../nyheter/index.php" title="Nyheter"><li>Nyheter</li></a>
-    <a href="../kontakt/index.php" title="Kontakt"><li>Kontakt</li></a>
+    <a href="../index.php" title="Prosjekt Scrödinger"><li>Hjem/Home</li></a>
+    <a href="../forskningsprosjekter/index.php" title="Forskningsprosjekter"><li>Forskningsprosjekter/Research projects</li></a>
+    <a href="../emneutvikler/index.php" title="Emneutvikler"><li>Emneutvikler/Course development</li></a>
+    <a href="../studentprosjekter/index.php" title="Studentprosjekter"><li>Studentprosjekter/Student projects</li></a>
+    <a href="../nyheter/index.php" title="Nyheter"><li>Nyheter/News</li></a>
+    <a href="../kontakt/index.php" title="Kontakt"><li>Kontakt/Contact</li></a>
   </ul>
 </div>
